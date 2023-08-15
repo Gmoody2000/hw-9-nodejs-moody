9cf8a7ce9a835017f45d95bb6e7ba5a56790e124
Gmoody2000 <127790461+Gmoody2000@users.noreply.github.com>
2023-08-15 18:16:31 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# README Generator
+
+## Description
+This is a command-line application that generates a professional README.md file based on user input. It uses the Inquirer package to prompt the user for information about their project and dynamically generates a high-quality README file.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+To install and use this README Generator, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory in your terminal.
+3. Run the following command to install the required dependencies:
+
+
+## Usage
+To generate a professional README file, follow these instructions:
+
+1. In your terminal, navigate to the project directory.
+2. Run the following command:
+
+3. Answer the prompts to provide information about your project.
+4. Once you've answered all prompts, a `README.md` file will be generated in the project directory.
+
+## Contributing
+If you'd like to contribute to this project, please follow these steps:
+
+1. Fork this repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them.
+4. Push your changes to your forked repository.
+5. Open a pull request to the original repository.
+
+## License
+This project is licensed under the [MIT License](LICENSE).
+
+## Questions
+For any questions or concerns, please feel free to reach out:
+
+- Email: garrettmoody011@gmail.com
+- GitHub: [Gmoody2000](https://github.com/Gmoody2000)
---
+++ index.mjs
@@ -0,0 +1,78 @@
+import inquirer from 'inquirer';
+
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'How do users install your project?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How should users use your project?',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines for your project?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache', 'GPL', 'ISC', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+    ];
+
+    inquirer.prompt(questions)
+    .then((answers) => {
+      const readmeContent = `
+        # ${answers.projectTitle}
+  
+        ## Description
+        ${answers.description}
+  
+        ## Installation
+        ${answers.installation}
+  
+        ## Usage
+        ${answers.usage}
+  
+        ## Contributing
+        ${answers.contributing}
+  
+        ## License
+        This project is licensed under the ${answers.license} license.
+        `;
+        
+      fs.writeFile('README.md', readmeContent, (err) => {
+        if (err) {
+          console.error('Error writing README file:', err);
+        } else {
+          console.log('README.md file generated successfully!');
+        }
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
